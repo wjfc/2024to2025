af6629539af24fc9d756e45deec44ce5edb8a511
jiweiwei <jiweiwei@chsvision.com>
2024-12-02 20:51:30 +0800

    新增webpack学习

---
+++ webpackStudy/webackBasic/src/index.js
@@ -0,0 +1,2 @@
+import "./assets/css/index.css"
+console.log(11)
---
+++ webpackStudy/webackBasic/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+}
